33c9cb46641d6d6021c161a3ea4e6cb334e14dd6
plumduffer <smjeffries@outlook.com>
2024-01-26 13:30:21 +1300

    feat: create basic password markup

---
+++ frontend/pages/password-login.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <main class="h-[100dvh] grid place-items-center">
+        <hgroup class="text-center">
+            <h1 class="text-5xl lg:text-7xl">Plum Duffer</h1>
+            <p>Coming soon</p>
+            <Input placeholder="Password" />
+            <Button>Unlock</Button>
+        </hgroup>
+    </main>
+</template>
