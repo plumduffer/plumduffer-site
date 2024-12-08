c40aba2913be1170f7f2ee51cc182ed2b935c5b1
plumduffer <stephen@plumduffer.dev>
2024-12-08 18:56:48 +1300

    lol delete

--- cms/src/access/isAdmin.ts
+++
@@ -1,7 +0,0 @@
-export const isAdmin = ({ req: { user } }) => {
-    return Boolean(user?.roles?.includes("admin"));
-};
-
-export const isAdminFieldLevel = ({ req: { user } }) => {
-    return Boolean(user?.roles?.includes("admin"));
-};
--- frontend/components/ui/payload-link/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as PayloadLink } from "./PayloadLink.vue";
